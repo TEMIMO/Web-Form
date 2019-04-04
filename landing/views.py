9ab7757602bc682a649a5efe7ef7e1c2f4bfb5af
Artemy <artemymif@gmail.com>
2019-04-04 23:46:12 +0300

    Initial commit

---
+++ landing/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .forms import SubscriberForm
+
+def landing(request):
+    form = SubscriberForm(request.POST or None)
+    if request.method == "POST" and form.is_valid():
+        form = form.save()
+    return render(request, 'landing/landing.html', locals())
