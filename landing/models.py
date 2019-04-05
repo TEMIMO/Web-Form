00538c472173a26822dcc14cf2c3e189ca7e62f9
Artemy <artemymif@gmail.com>
2019-04-05 09:54:25 +0300

    I commit it

--- landing/models.py
+++ landing/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Subscribers(models.Model):
     email = models.EmailField()
     name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return "%s %s" % (self.name, self.email)
